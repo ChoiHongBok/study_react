b09a20b9ff12af3718f74214f88fe48d48708838
ChoiHongBok <ghdqhr0105@naver.com>
2024-03-22 00:01:16 +0900

    useState 사용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    // ES6 destructuring
+    // let [a, b] = [10, 100] : a = 10, b = 100
+
+    // 변수 : 변수는 데이터가 변경되어도 자동 렌더링이 되지 않는다.
+    let topTitle = 'React 연습'
+
+    // state 는 데이터가 변경되면 HTML 이 자동으로 랜더링된다.
+    let [title, changeTitle] = useState(['강남 카페', '강남 돈까스']);
+
     return (
         <div classname="txt_middle">
             <div className="nav_black">
-                <dov>React 연습</dov>
+                <div style={{color: "red", fontSize: "30px"}}>{topTitle}</div>
+            </div>
+            <div className="list">
+                <h3>{title[0]}</h3>
             </div>
         </div>
     );
