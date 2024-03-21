55a1401843db015b1db9fba8e41684d593ef0b33
ChoiHongBok <ghdqhr0105@naver.com>
2024-03-21 23:22:16 +0900

    초기화 작업

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
-
 import './App.css';
 
 function App() {
     return (
-        <div classname="App">
-            <header className="App-header">
-                <img src={logo} className="APp-logo" alt="{logo}"/>
-                <p>안녕하세요</p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-            </header>
+        <div classname="txt_middle">
+            <div className="nav_black">
+                <dov>React 연습</dov>
+            </div>
         </div>
     );
 }
