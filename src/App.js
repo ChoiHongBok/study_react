12af5b819b2512f1b05370f3bb4462e6b640b3c2
ChoiHongBok <ghdqhr0105@naver.com>
2024-03-24 20:07:50 +0900

    배열 추가 및 삭제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, {useRef} from 'react';
+import React, {useState, useRef} from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from "./Counter";
 import InputSample from "./InputSample";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
-    const users = [
+    const [users, changeUsers] = useState([
         {
             id: 1,
             username: 'velopert',
@@ -22,24 +23,65 @@ function App() {
             username: 'liz',
             email: 'liz@example.com'
         }
-    ];
+    ]);
+
+    let [createUser, changeCreateUser] = useState({
+        userName: "",
+        email: ""
+    });
+
+    let {userName, email} = createUser;
+
+    const onChange = (e) => {
+        const {name, value} = e.target;
+
+        changeCreateUser({
+            ...createUser,
+            [name]: value
+        });
+    };
 
     const nextId = useRef(4); // useRef : nextId.current 에 기본값이 된다
-
     const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username: userName,
+            email: email
+        }
+
+        // changeUsers([
+        //     ...users,
+        //     user
+        // ]);
+        changeUsers(users.concat(user));
+
+        changeCreateUser({
+            userName: "",
+            email: ""
+        });
+
         nextId.current += 1;
+    };
+
+    const onRemove = (id) => {
+        changeUsers(
+            users.filter(user => {
+                return user.id !== id;
+            })
+        );
     };
 
     return (
         <div>
-            <Wrapper>
-                <Hello name='최홍복' color='red' isSpecial={true} />
-                <Hello color='blue' isSpecial /> {/* Default true */}
-                <Hello />
-            </Wrapper>
-            <Counter />
-            <InputSample />
-            <UserList users={users} />
+            {/*<Wrapper>*/}
+            {/*    <Hello name='최홍복' color='red' isSpecial={true} />*/}
+            {/*    <Hello color='blue' isSpecial /> /!* Default true *!/*/}
+            {/*    <Hello />*/}
+            {/*</Wrapper>*/}
+            {/*<Counter />*/}
+            {/*<InputSample />*/}
+            <CreateUser userName={userName} email={email} onChange={onChange} onCreate={onCreate} />
+            <UserList users={users} onRemove={onRemove} />
         </div>
     );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-function User({user}) {
+function User({user, onRemove}) {
     return (
         <div>
             <b>{user.username}</b> <span>{user.email}</span>
+            <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     )
 }
 
-function UserList({users}) {
+function UserList({users, onRemove}) {
     return (
         <div>
             {users.map(user => (
-                <User user={user} key={user.id} />
+                <User user={user} onRemove={onRemove} key={user.id} />
             ))}
             {/* 아이디가 없는 경우에는 */}
             {/*{users.map((user, index) => (*/}
